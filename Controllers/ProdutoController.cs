72ec169b92834701b652c0dd3fdad672642bbdfb
Douglas Alves <douglas_alvrodrigues@outlook.com>
2020-01-07 14:50:31 -0300

    Loja Virtual

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController: Controller
+    {
+
+        public ActionResult Visualizar()
+        {
+            return new ContentResult() { Content = "<h3>Produto - Visualizar</h3>", ContentType = "text/html" };
+        }
+    }
+}
